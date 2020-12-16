e8d5b9b1eaa3071dd8e2e86e55f02e5b10404836
Igor <nrovo7@gmail.com>
2020-12-16 20:57:43 +0200

    In this commit I've created an IAlphabet interface which includes obligatory property returning an array of chars. In inherited classes this property is containing an actual language alphabet. Two inherited classes was added for English and Russian languages.

---
+++ MessageFromJuliusCaesar/Alphabet/EnglishAlphabet.cs
@@ -0,0 +1,12 @@
+﻿namespace MessageFromJuliusCaesar.Alphabet
+{
+    internal class EnglishAlphabet : IAlphabet
+    {
+        public char[] Alphabet =>
+            new char[26]
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z'
+            };
+    }
+}
